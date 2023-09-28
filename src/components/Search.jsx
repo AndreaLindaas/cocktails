b9055761dfa02886894b3e07bdc0d9f740def1f3
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2023-09-28 14:05:19 +0200

    added heading and searchbar

---
+++ src/components/Search.jsx
@@ -0,0 +1,8 @@
+export default function Search() {
+
+    return ( <>
+    <div className="search">
+        <input type="text" className="search-bar" placeholder="Search.." />
+      </div>
+    </>)
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+
+import Search from '../components/Search'
+import './Home.css'
+
+function Home() {
+ 
+
+  return (
+    <>
+    <h1>Cocktails</h1>   
+    <Search/>
+ </>
+  )
+}
+
+export default Home
