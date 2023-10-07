5c21881fe7efb316b39e3315e7ef19697033444a
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2023-10-07 21:02:56 +0200

    added card

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,26 +1,19 @@
 import { useEffect, useState } from "react";
+import Card from "../Card/Card";
+export default function Cards() {
+  const [cocktails, setCocktails] = useState([]);
 
+  useEffect(() => {
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=A")
+      .then((response) => response.json())
+      .then((result) => setCocktails(result.drinks));
+  }, []);
 
-export default function Cards (){
-    const [cocktails, setCocktails] = useState([]);
+  const renderCocktails = () => {
+    return cocktails.map((c, index) => {
+      return <Card cocktail={c} key={index} />;
+    });
+  };
 
-    useEffect(()=>{
-        fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=A")
-        .then((response)=>response.json())
-        .then((result)=> setCocktails(result.drinks))
-    },[])
-
-const renderCocktails=()=>{
-    return cocktails.map((cocktail,index)=>{
-        return(<li key={index} >
-            <img src={cocktail.strDrinkThumb} />
-            <div>{
-        cocktail.strAlcoholic != "Alcoholic" ? cocktail.strAlcoholic : ""
-      }</div>
-            <span>
-            {cocktail.strDrink}</span></li>)
-    })
-}
-
-    return(<ul className="cocktails">{renderCocktails()}</ul>)
+  return <ul className="cocktails">{renderCocktails()}</ul>;
 }
